55a3d8460b959f673fb1e960a75f00c2f715d7fa
ibnurizal1978 <ibnurizal@gmail.com>
2026-04-17 08:28:35 +0700

    change price item

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -47,6 +47,13 @@ export default function Pricing() {
                             </li>
                             <li>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <polyline points="18 6 6 18 12 12 6 6 18 18"></polyline>
+                                </svg>
+                                Document Upload (Certificates, Work Contract, etc)
+                            </li>
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
                                     stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                     <polyline points="20 6 9 17 4 12"></polyline>
                                 </svg>
@@ -58,13 +65,6 @@ export default function Pricing() {
                                     <polyline points="20 6 9 17 4 12"></polyline>
                                 </svg>
                                 Multiple Reminder Notifications
-                            </li>
-                            <li>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-                                    stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                    <polyline points="20 6 9 17 4 12"></polyline>
-                                </svg>
-                                Document Upload (Certificates, Work Contract, etc)
                             </li>
                             <li>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
